aefb1f972788b93995f83a642bc5d047954a314d
moahmed hussein <mh95568@gmail.com>
2022-10-16 02:52:31 +0200

    third commit queuingTime

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,51 @@
+import java.util.Queue
+import kotlin.math.abs
+
 var clock: Int = 0
 var ableBegin: Int = 0
 var ableSt: Int = 0
 var ableEnd: Int = 0
+var ableIdle : Int= 0
 var bakerBegin: Int = 0
 var bakerSt: Int = 0
 var bakerEnd: Int = 0
-var check:Boolean = true
+var bakerIdle : Int = 0
+var randomNumber : Int = 0
+var iAt :Int = 0
+var rnSt:Int = 0
+var queueTime : Int= 0
+var timeSpent :Int = 0
+
 
 fun main(args: Array<String>) {
-
     for (i in 0..7){
-        println("enter IAN")
         if (i!=0){
-            clock += randomProbability(readln().toInt())
-
+            println("enter IAN")
+            randomNumber =readln().toInt()
+            iAt = randomProbability(randomNumber)
+            clock += iAt
         }
-        println(clock)
 
         if (ableOrbaker()){
-            println("enter able Random")
-            ableEnd +=AbleProbability(readln().toInt())
-            println(ableEnd)
+            println("enter able St")
+            rnSt = readln().toInt()
+            queueTime = if ((clock-ableEnd)< 0) abs(clock-ableEnd) else 0
+            ableBegin = if(clock > ableEnd) clock else ableEnd
+            ableSt = AbleProbability(rnSt)
+            ableEnd = ableBegin + ableSt
         }
         else{
-            println("enter backer random")
-            bakerEnd +=BakerProbability(readln().toInt())
-            println(bakerEnd)
+            println("enter Baker St")
+            rnSt = readln().toInt()
+            queueTime = if ((clock-bakerEnd)< 0) abs(clock-bakerEnd) else 0
+            bakerBegin = if(clock > bakerEnd) clock else bakerEnd
+            bakerSt = BakerProbability(rnSt)
+            bakerEnd = bakerBegin + bakerSt
         }
+        println("id\tRN-IAN\tiAt\tClock\tRN-St\tAbleBegin\tAbleST\tAbleEnd\tBakerBegin\tBakerST\tBakerEnd\tQueuingTime")
+        println("${i+1}\t\t$randomNumber\t $iAt\t  $clock\t  $rnSt\t    $ableBegin\t      $ableSt\t        $ableEnd\t\t$bakerBegin\t\t\t$bakerSt\t\t$bakerEnd\t\t\t$queueTime")
     }
-    println(clock)
+
 
 
 
@@ -65,9 +82,7 @@ fun BakerProbability(x: Int): Int {
 }
 
 fun ableOrbaker() : Boolean{
-    println("able End $ableEnd")
-    println("backe End $bakerEnd")
-    println("clock $clock")
+
     if(clock == ableEnd ){
         return true
     }else if (clock > ableEnd && clock >bakerEnd){
